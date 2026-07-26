ed3b7ea0d23d543998dfc453d9c690db41eb1f96
mostafazareiedarki-ai <mostafa.zareie.darki@gmail.com>
2026-07-26 22:39:56 +0330

    Add files via upload

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mellalchem-v2';
+const CACHE = 'mellalchem-v13';
 const FILES = ['./', './index.html', './manifest.json', './assets/icon.png', './assets/icon-192.png', './assets/icon-512.png', './assets/splash.jpg', './assets/dev-photo.jpg', './assets/B-NAZANIN.TTF', './assets/B_Titr_Bold_0.ttf'];
 
 self.addEventListener('install', e=>{
